04a68338939a58abaf321cfc06cccd98fd0ed403
Zizzo11 <zizzo.11.79@gmail.com>
2026-01-06 22:32:31 +0100

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-function generatepoem(event) {
-    event.preventDefault();
+function displayPoem(response) {
     new Typewriter('#poem', {
-  strings: "roses are red, violets are blue, AI generates poems, just for you.",
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "|",
 });
+}
+
+
+
+function generatepoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "4bd7a33cfa794e02bob9f9a9tf20b54c";
+
+let context = " You are a talented persian poet known for your creativity and vivid imagery. your mission is generate 4 line poem in basic HTML. the persian poem is from right to left. write the poet who the poem belongs to at the end of the poem. make sure to follow the user instructions." ;
+let prompt = `user instructions: Generate a persian poem about ${instructionsInput.value}.`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+axios.get(apiUrl).then(displayPoem);
+
+
+
+
+    
 
 }
 
